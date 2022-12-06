2907ff62eef31a4e19c007f28f81ebcf732d562f
lucamattana <luca.mattbes@gmail.com>
2022-12-06 20:31:05 -0300

    Pasa el Nav a App para que sea global

--- Front/src/Pages/Home/Home.jsx
+++ Front/src/Pages/Home/Home.jsx
@@ -1,14 +1,11 @@
-import { Container } from '@chakra-ui/react'
+
 import React from 'react'
 import Hero from '../../Components/Hero/Hero'
-import Nav from '../../Components/Nav/Nav'
+
 
 const Home = () => {
   return (
-    <Container maxW="container.xl">
-      <Nav />
       <Hero />
-    </Container>
   )
 }
 

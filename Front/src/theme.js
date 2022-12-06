bf5515f1c32f7f273aea81103dfb2a5e60b3f203
felipeurien <urienfelipe@gmail.com>
2022-12-06 20:56:15 -0300

    creado de Nav y logo y title de la pestaña

--- Front/src/theme.js
+++ Front/src/theme.js
@@ -1,11 +1,23 @@
-import { extendTheme, theme } from '@chakra-ui/react'
+import { extendTheme, theme } from "@chakra-ui/react";
 
 export default extendTheme({
   colors: {
     primary: {
       ...theme.colors.pink,
-      200: '#F6ACA3',
-      300: '#FF6B6B'
-    }
-  }
-})
+      200: "#F6ACA3",
+      300: "#FF6B6B",
+    },
+  },
+  global: {
+    a: {
+      _hover: {
+        textDecoration: "none",
+      },
+    },
+    p: {
+      _hover: {
+        textDecoration: "none",
+      },
+    },
+  },
+});

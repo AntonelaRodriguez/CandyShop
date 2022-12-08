0675c25586ed579ad5ae142b441390758176ab08
felipeurien <urienfelipe@gmail.com>
2022-12-08 13:56:33 -0300

    Creado de basico Forms y sus archivos

--- Front/src/Pages/Create/Create.jsx
+++ Front/src/Pages/Create/Create.jsx
@@ -35,46 +35,6 @@ import {
 //stock
 //disponibilidad 
 
-function validate(input) {
-    let errors = {}
-
-    if (!input.name) {
-        errors.name = "Name is required";
-      } else if (input.name.length > 50) {
-        errors.name = "Name is too long";
-      }
-  
-      if (!input.description) {
-        errors.description = "Description is required ";
-      } else if (input.description.length > 1500) {
-        errors.description = "Description is too long. (Max = 1500 characters)";
-      }
-  
-      if (!input.rating) {
-        errors.rating = "Rating is required";
-      } else if (input.rating > 5 || input.rating < 0) {
-        errors.rating = "Rating must range between 0 to 5";
-      }
-  
-      if (!input.released) {
-        errors.released = "Date of release is required";
-      } else if (input.released.length < 10) {
-        errors.released = "Date of release is to long";
-      }
-      if (!input.img) {
-        errors.img = "Image URL is required";
-      }
-  
-      if (!input.genre[0]) {
-        errors.genre = "At least one Genre is required ";
-      }
-  
-      if (!input.platforms[0]) {
-        errors.platforms = "At least one Platform is required";
-      }
-  
-      return errors;
-}
 
 const Create = () => {
     const [ errors, setErrors ] = useState({})
@@ -89,17 +49,6 @@ const Create = () => {
         price: [],
         stock: [],
     })
-
-    function handleChange(e){
-        setInput({
-            ...input,
-            [e.target.name] : e.target.value
-        })
-        setErrors(validate({
-            ...input,
-            [e.target.name]: e.target.value
-        }))
-    }
 
     return(
         <form action='submit'>

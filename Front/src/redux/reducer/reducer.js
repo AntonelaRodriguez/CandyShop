a676ec9f2d4318c416e9589185041cde37fe96c0
lucamattana <luca.mattbes@gmail.com>
2022-12-08 15:07:59 -0300

    get all y search en redux

--- Front/src/redux/reducer/reducer.js
+++ Front/src/redux/reducer/reducer.js
@@ -1,9 +1,23 @@
-const initialState = {}
+import { ALL_PRODUCTS, SEARCH_CANDY } from "../actions/actions"
+
+const initialState = {
+  products: []
+}
 
 const reducer = (state = initialState, { type, payload }) => {
-  switch ("") {
-    case "das":
-      break
+  switch (type) {
+    case ALL_PRODUCTS: {
+      return ({
+        ...state,
+        products: payload
+      })
+    }
+    case SEARCH_CANDY: {
+      return ({
+        ...state,
+        products: payload
+      })
+    }
 
     default:
       return state

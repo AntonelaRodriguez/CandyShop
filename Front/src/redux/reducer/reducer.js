2ca704c012f0c77d1163f2602abbdd9fd1b321a1
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2022-12-11 19:44:41 -0300

    arreglo de error en reducers

--- Front/src/redux/reducer/reducer.js
+++ Front/src/redux/reducer/reducer.js
@@ -5,12 +5,11 @@ import {
   DETAILS_PRODUCT,
   POST_PRODUCT,
   ALL_CATEGORIES,
- SET_FILTERS,
+  SET_FILTERS,
   APPLY_FILTERS,
   EDIT_PRODUCT,
   DELETED_PRODUCT
 } from '../actions/actions'
-
 
 const initialState = {
   products: [],
@@ -38,9 +37,8 @@ const initialState = {
     'Ferrero',
     'Unknown'
   ],
-   filters: { tacc: "TACC", brand: "BRAND", category: "CATEGORY" },
+  filters: { tacc: 'TACC', brand: 'BRAND', category: 'CATEGORY' }
 }
-
 
 const reducer = (state = initialState, { type, payload }) => {
   switch (type) {
@@ -103,18 +101,17 @@ const reducer = (state = initialState, { type, payload }) => {
 
     case DELETED_PRODUCT:
       return { ...state }
+
+    case SET_FILTERS:
+      return {
         ...state,
-      };
-      case SET_FILTERS:
-        return {
-          ...state,
-          filters: payload,
-        };
-      case APPLY_FILTERS:
-        return {
-          ...state,
-          products: payload,
-        };
+        filters: payload
+      }
+    case APPLY_FILTERS:
+      return {
+        ...state,
+        products: payload
+      }
     default:
       return state
   }

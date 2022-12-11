fac4c0a229d16ab40ce9ea5b36026658eda67886
Agustin Barrionuevo <barrionuevoagustin289@gmail.com>
2022-12-11 13:23:24 -0300

    cambios en category model y product model para la db

--- Back/src/models/Product.js
+++ Back/src/models/Product.js
@@ -1,78 +1,83 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define("Product", {
-    id:{
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true
-    },
-    name: {
+  sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          notEmpty: true
-        }
-    },
-    description: {
-       type: DataTypes.STRING,
-       allowNull: false,
-       validate: {
-        notEmpty: true
-      }
-    },
-    price:{
+          notEmpty: true,
+        },
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      price: {
         type: DataTypes.DECIMAL,
         allowNull: false,
         validate: {
-            notEmpty: true
-        }
-    },
-    availability:{
-       type: DataTypes.BOOLEAN,
-       defaultValue: true
-    },
-    image:{
-        type: DataTypes.STRING,
-        allowNull:false 
-    },
-    stock:{
-        type:DataTypes.INTEGER,
-        allowNull: false
-    },
-    brand: {
-      type: DataTypes.ENUM([
-        "Arcor",
-        "Bagley",
-        "Milka",
-        "Mogul",
-        "Aguila",
-        "Bon o Bon",
-        "Cofler",
-        "Terrabusi",
-        "Topline",
-        "Tofi",
-        "Godet",
-        "Nestle",
-        "Felfort",
-        "Billiken",
-        "Georgalos",
-        "Bonafide",
-        "Jorgito",
-        "Trident",
-        "Ferrero",
-        "Unknown",
-      ]),
-      defaultValue: "Unknown",
-    },
-    tacc: {
+          notEmpty: true,
+        },
+      },
+      availability: {
         type: DataTypes.BOOLEAN,
-        allowNull: false
-    }
+        defaultValue: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.ENUM([
+          "arcor",
+          "felfort",
+          "bagley",
+          "georgalos",
+          "mogul",
+          "tofi",
+          "godet",
+          "topline",
+          "aguila",
+          "bon o bon",
+          "cofler",
+          "unknown",
+        ]),
+        defaultValue: "unknown",
+      },
+      tacc: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
     },
     {
-      timestamps: false
+      timestamps: false,
     }
   );
 };
+
+
+//brands         
+  // "bonafide",
+  // "milka",
+  // "jorgito",
+  // "trident",
+  // "ferrero",
+  // "terrabusi",
+  // "nestle",
+  // "billiken",

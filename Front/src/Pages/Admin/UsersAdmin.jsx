e841c8f3eb14751e36018ed99a4a243bcb7d2622
felipeurien <urienfelipe@gmail.com>
2022-12-16 20:00:34 -0300

    creado admin

---
+++ Front/src/Pages/Admin/UsersAdmin.jsx
@@ -0,0 +1,11 @@
+import SimpleSidebar from "./src/NavAdmin/NavAdmin";
+const UsersAdmin = () => {
+  return (
+    <>
+      <SimpleSidebar />
+      <div>UsersAdmin</div>
+    </>
+  );
+};
+
+export default UsersAdmin;

b08c70cd790097399706c5fd4d7c54d62d7a7b88
Antonela <antonelabelenrodriguez@gmail.com>
2023-01-10 22:42:16 -0300

    Cambio estilos al footer y al candyStores, controlo form de contact us y agrego funconalidad del localStorage, arreglo bug de compra mayor a $ 5000

--- Front/src/Pages/CandyStores/CandyStores.jsx
+++ Front/src/Pages/CandyStores/CandyStores.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {
     Container,
+    Flex,
+    Box,
     Link,
     SimpleGrid,
     Stack,
@@ -16,16 +18,29 @@ import {
 
 const CandyStores = () => {
     return(
+        <Flex as="nav" align="center" justify="center" wrap="wrap" py={6} w="full">
+            <Box
+        w="full"
+        direction={{ base: "column", sm: "column", md: "row", lg: "row" }}
+        justifyContent="space-between"
+        borderRadius="md"
+        height="full"
+        margin="auto"
+        border='1px solid #F6ACA3'
+        boxShadow="xl"
+            >
         <Container direction='column' align='center' justify='center'>
-            <Heading fontWeight={700} fontSize='4xl' m={'1rem'} mb={'2rem'}> Our Candy Stores: </Heading>
-            <Text fontSize='2xl' m={'1rem'}>We have stores all over the country</Text>
+            <Heading color={'tomato'} fontWeight={700} fontSize='4xl' m={'1rem'} mb={'2rem'} mt={'2rem'}> Our Candy Stores: </Heading>
+            <Text fontSize='2xl' m={'1rem'}>We have stores all over the country.</Text>
             <Text fontSize='2xl' m={'1rem'} mb={'2rem'}>Take a look and come to vist someday!</Text>
-            <AspectRatio ratio={17 / 10}>
+            <AspectRatio ratio={19 / 12} border='7px solid #F6ACA3' borderRadius="md" boxShadow="2xl">
                 <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3473988.218606696!2d-65.30681522998098!3d-28.928753403277007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sarcor%20tienda%20de%20golosinas!5e0!3m2!1ses-419!2sar!4v1673283016828!5m2!1ses-419!2sar">
                 </iframe>
             </AspectRatio>
-            <Text color={'tomato'} fontSize='2xl' m={'1rem'} mt={'2rem'}>All our products are available in stores too!</Text>
+            <Text color={'tomato'} fontSize='2xl' m={'1rem'} mt={'2rem'} mb={'2rem'}>All our products are available in stores too!</Text>
         </Container>
+            </Box>
+        </Flex>
     )
 };
 

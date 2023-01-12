89ccc69a21a9e86156780b96f3076763c84ce147
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-01-12 11:53:16 -0300

    url deploy

--- Front/src/redux/actions/actions.js
+++ Front/src/redux/actions/actions.js
@@ -41,8 +41,8 @@ export const CHANGE_SUBSCRIPTION = "CHANGE_SUBSCRIPTION";
 export const PURCHASED_PRODUCTS = "PURCHASED_PRODUCTS";
 
 /* const url = 'https://deploydbcandy-production.up.railway.app' //usar url para db deployada */
-//const url = "https://candyshop-production.up.railway.app";  // usar url para db deployada
-const url = "http://localhost:3001"; //para usar la db local poner localhost en vez de url
+const url = "https://candyshop-production.up.railway.app";  // usar url para db deployada
+/* const url = "http://localhost:3001"; //para usar la db local poner localhost en vez de url */
 
 export const getAllProducts = () => {
   return async function (dispatch) {
